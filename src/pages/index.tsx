cbfe583fc27e4db0d998aaf245325109edffb345
Danny Kimble <daniel.kimble1985@gmail.com>
2023-02-10 00:21:59 -0600

    Removes Header component, rolled into Sidebar

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,6 @@ import Head from "next/head";
 import { useSession } from "next-auth/react";
 
 import { api } from "../utils/api";
-import Header from "../ components/Header";
 import Sidebar from "../ components/Sidebar";
 
 const Home: NextPage = () => {
